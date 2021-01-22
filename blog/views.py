c2905356d65b9d3fb13ea3461adab618828a17b0
Ramathmika <ramathmikavs1999@gmail.com>
2021-01-22 15:50:36 +0530

    Added Add and Edit pages functionalities

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def post_detail(request, pk):
 	return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-	if request.method == "POST"
+	if(request.method == "POST")
 		form = PostForm(request.POST)
 		if(form.is_valid()):
 			post = form.save(commit=False)
@@ -27,7 +27,7 @@ def post_new(request):
 
 def post_edit(request,pk):
 	post = get_object_or_404(Post, pk=pk)
-	if request.method = "POST":
+	if request.method == "POST":
 		form = PostForm(request.POST, instance=post)
 		if form.is_valid():
 			post = form.save(commit=False)
